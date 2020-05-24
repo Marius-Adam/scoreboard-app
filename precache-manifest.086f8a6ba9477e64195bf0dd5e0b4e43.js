37e29caaee5e99a66b31b91d2240f538c940d58e
Marius-Adam <marius.c.adam@gmail.com>
2020-05-24 15:02:18 +0100

    Updates

---
+++ precache-manifest.086f8a6ba9477e64195bf0dd5e0b4e43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1ca77e9ce32105ae58cdac8440159b2",
+    "url": "/scoreboard-app/index.html"
+  },
+  {
+    "revision": "7062ce87f94507e3ecff",
+    "url": "/scoreboard-app/static/css/main.a9401042.chunk.css"
+  },
+  {
+    "revision": "07e1876c4af376dac368",
+    "url": "/scoreboard-app/static/js/2.669d4b7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/scoreboard-app/static/js/2.669d4b7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7062ce87f94507e3ecff",
+    "url": "/scoreboard-app/static/js/main.aaed7b41.chunk.js"
+  },
+  {
+    "revision": "36e916f01f736ee18bcb",
+    "url": "/scoreboard-app/static/js/runtime-main.b663519e.js"
+  }
+]);
